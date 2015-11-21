930617976caf0e96ce5783c277178d15acf61c25
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2015-11-21 23:47:18 +0100

    adjust structure and wrapper script

--- gen.sh
+++ gen.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-TEXFILE="guide.tex"
-HTMLFILE="`echo ${TEXFILE} | cut -d '.' -f 1`.html"
+TEXFILE="latex/guide.tex"
+HTMLFILE="`echo ${TEXFILE} | cut -d '/' -f 2 | cut -d '.' -f 1`.html"
 
 if [ ${#} -ne 1 ]
 then
-    echo "sh ${0} <action>"
-    echo "actions:"
-    echo "  pdf     - create pdf document"
+    echo "${0} <target>"
+    echo "targets:"
     echo "  html    - create one html document"
+    echo "  pdf     - create pdf document"
     echo "  clean   - clean files and directory"
     exit 1337
 fi
 
 make_final_html()
 {
-    cat header.html > ${HTMLFILE}
-    sed '1,23d' guide/${HTMLFILE} | sed '$d' | sed '$d' \
+    cat html/header.html > ${HTMLFILE}
+    sed '1,23d' latex/guide/${HTMLFILE} | sed '$d' | sed '$d' \
         >> ${HTMLFILE}
-    cat footer.html >> ${HTMLFILE}
+    cat html/footer.html >> ${HTMLFILE}
 }
 
 if [ "${1}" = "pdf" ]
@@ -32,7 +32,7 @@ then
     make_final_html
 elif [ "${1}" = "clean" ]
 then
-    rm -rf *.pdf *.toc *.log *.aux *.out *.exs ${HTMLFILE} guide
+    rm -rf *.pdf *.toc *.log *.aux *.out *.exs ${HTMLFILE} latex/guide guide.pdf
 else
     echo "mount /dev/brain"
     exit 1337
