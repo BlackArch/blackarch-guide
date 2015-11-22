2e92fa8e91593d47d7f3a7d71e9755d2f3b714aa
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2015-11-22 12:25:49 +0100

    update header and wrapper script

--- sync.sh
+++ sync.sh
@@ -2,16 +2,16 @@
 
 site='blackarch.org'
 rdir='/home/nginx/var/www'
+
 if [ ${#} -ne 1 ]
 then
-	usr=$USER
+    usr=$USER
 else
-	usr="${1}"
+    usr="${1}"
 fi
 
 cd "`dirname "$0"`"
 
 # Delete manually or something.
 rsync --rsh="ssh -A -l ${usr}" --chmod "a+rX,ug+w" -avz \
-	guide.html \
-	"$site:/$rdir"
+    guide.html guide.pdf "$site:/$rdir"
