2267ad17ca9074736bbf18f9a1e7adef560c96cc
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2016-04-18 23:50:29 +0200

    rename files

--- sync.sh
+++ sync.sh
@@ -3,15 +3,14 @@
 site='blackarch.org'
 rdir='/home/nginx/var/www'
 
-if [ ${#} -ne 1 ]
+if [ $# -ne 1 ]
 then
     usr=$USER
 else
     usr="${1}"
 fi
 
-cd "`dirname "$0"`"
+cd "`dirname "${0}"`"
 
 # Delete manually or something.
-rsync --rsh="ssh -A -l ${usr}" --chmod "a+rX,ug+w" -avz \
-    guide.html guide.pdf "$site:/$rdir"
+rsync --rsh="ssh -A -l ${usr}" --chmod "a+rX,ug+w" -avz *.pdf "${site}:/${rdir}"
